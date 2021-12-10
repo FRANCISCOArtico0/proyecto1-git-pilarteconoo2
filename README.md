230bffac397eb1171e7abd4f53860986b736a8b7
Francisco Artico <franciscoartico0@gmail.com>
2021-12-09 21:50:32 -0300

    cowsay finnally

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-*** Poyecto Hola Mundo! ***
+## Poyecto Hola Pilar Mundo!
+
+Proyecto Hola mundo en node, pero con una vaquita.
+
+ejecutar con
+
+
+```
+node main.js
+```
--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hola PilarTecno!');
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hola Pilar tecno",
+    e : "oO",
+    T : "U "
+}));
